877ab9873a9fa5ae0b060de667b391f54253d4c7
yehan432 <yh432@sina.com>
2017-05-23 15:48:15 +0800

    增加胶囊下落和判断游戏结束（细碎版）

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Virus from './components/Virus';
 import Capsule from './components/Capsule';
 import './App.css';
 
+var grids = new Array();
+let top;
+let bottom;
+
 class App extends Component {
 
   state = {
@@ -12,8 +16,8 @@ class App extends Component {
 
   // 随机开局
   updateGrids = () => {
+    grids = [];
     let blank;
-    var grids = new Array();
     for (var i = 0; i < 16; i++) {
       for (var j = 0; j < 8; j++) {
         if (i > 4) {
@@ -41,33 +45,91 @@ class App extends Component {
         }
       }
     }
-    grids[3] = { type: 'capsule', color: 'yellow', x: 3, y: 0 ,rotate:'top'};
-    grids[11] = { type: 'capsule', color: 'red', x: 3, y: 1 ,rotate:'bottom'};
+    // 第一个药丸
+    top = { x: 3, y: 0, rotate: 'top' };
+    bottom = { x: 3, y: 1, rotate: 'bottom' };
+    top = this.makeCapsule(top);
+    bottom = this.makeCapsule(bottom);
+    // 开始计时器
+    this.timer = setInterval(function () {
+      this.fall();
+    }.bind(this), 1000);
     this.setState({
       grids: grids
     })
   }
 
-  //画格子
-  getGrid = (grid) => {
+  // 生成半个药丸
+  makeCapsule = (capsule) => {
+    // 判断当前格子是否为空,如果不为空则不能生成药丸
+    if (grids[capsule.x + (capsule.y * 8)].type != 'blank') {
+      // 停止计时器
+      clearInterval(this.timer);
+      alert('游戏结束！');
+      return;
+    }
+    // 生成随机颜色的药丸
+    let random = parseInt(Math.random() * 3);
+    let color;
+    switch (random) {
+      case 0:
+        color = 'red';
+        break;
+      case 1:
+        color = 'blue';
+        break;
+      case 2:
+        color = 'yellow';
+        break;
+    }
+    capsule = { type: 'capsule', color: color, x: capsule.x, y: capsule.y, rotate: capsule.rotate };
+    grids[capsule.x + (capsule.y * 8)] = capsule;
+    return capsule;
+  }
+
+  // 画格子
+  paintGrid = (grid) => {
     switch (grid.type) {
       case 'blank':
         return <Virus key={(grid.x + 1) + (grid.y * 8)} type={grid.type} />;
       case 'virus':
         return <Virus key={(grid.x + 1) + (grid.y * 8)} type={grid.color} />;
       case 'capsule':
-        return <Capsule key={(grid.x + 1) + (grid.y * 8)} type={grid.color} rotate={grid.rotate}/>;
+        return <Capsule key={(grid.x + 1) + (grid.y * 8)} type={grid.color} rotate={grid.rotate} />;
     }
   }
 
+  // 消除格子
+  clearGrid = (x, y) => {
+    grids[x + (y * 8)] = { type: 'blank', x: x, y: y };
+  }
+
+  // 下落
+  fall = () => {
+    // 如果下面的格子为空，则下降一格
+    if (grids[bottom.x + (bottom.y + 1) * 8].type == 'blank') {
+      grids[bottom.x + (bottom.y + 1) * 8] = { type: 'capsule', color: bottom.color, x: bottom.x, y: bottom.y + 1, rotate: bottom.rotate };
+      grids[top.x + (top.y + 1) * 8] = { type: 'capsule', color: top.color, x: top.x, y: top.y + 1, rotate: top.rotate };
+      this.clearGrid(top.x, top.y);
+      bottom.y++;
+      top.y++;
+    } else {
+      top = { x: 3, y: 0, rotate: 'top' };
+      bottom = { x: 3, y: 1, rotate: 'bottom' };
+      top = this.makeCapsule(top);
+      bottom = this.makeCapsule(bottom);
+    }
+    this.setState({
+      grids: grids
+    })
+  }
   render() {
     const { grids } = this.state;
     return (
       <div>
         <Bottle>
-          {grids.map((grid) => (this.getGrid(grid)))}
+          {grids.map((grid) => (this.paintGrid(grid)))}
         </Bottle>
-        {/* 更新state的例子， 可删 */}
         <button onClick={this.updateGrids}>开始游戏</button>
       </div>
     );
--- src/components/Capsule/index.js
+++ src/components/Capsule/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './style.css';
 
 class Capsule extends Component {
-
   render() {
     const { type, rotate } = this.props;
     return (
